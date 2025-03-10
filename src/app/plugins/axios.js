4c423a6f4563dff253b96c7afb93f37cbcdbbc0d
ADolmos194 <AyltonDolmos194@gmail.com>
2025-03-10 15:58:05 -0500

    railway

--- src/app/plugins/axios.js
+++ src/app/plugins/axios.js
@@ -1,10 +1,10 @@
-// @/plugins/axios.js
 import axios from 'axios';
 
-// URL base de la API
-export const api_url = 'http://127.0.0.1:8080/';
+
+export const api_url = 'https://proyectometeoritobackend-production.up.railway.app/';
 
 // Crea la instancia de axios
 export const axiosIns = axios.create({
     baseURL: api_url,
 });
+
