dd6bb684ddd928813a68ee5624ad9b9376bb9189
ADolmos194 <AyltonDolmos194@gmail.com>
2025-03-24 02:26:12 -0500

    nuevo frontend

--- src/app/plugins/axios.js
+++ src/app/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-export const api_url = 'http://127.0.0.1:8000/'; /*'http://127.0.0.1:8000/'; https://curious-alice-proyecto-meteorito-backend-030d8df1.koyeb.app/';*/
+export const api_url = 'https://curious-alice-proyecto-meteorito-backend-030d8df1.koyeb.app/'; /*'http://127.0.0.1:8000/'; https://curious-alice-proyecto-meteorito-backend-030d8df1.koyeb.app/';*/
 
 export const axiosIns = axios.create({
     baseURL: api_url,
